99b6fae0507646e0f0900d34c700510f90293912
drikin <drikin@gmail.com>
2009-08-05 20:49:45 -0700

    move dir and update script

--- skype2prowl/skype2prowl.py
+++ skype2prowl/skype2prowl.py
@@ -9,13 +9,19 @@ Copyright (c) 2009 drikin.com. All rights reserved.
 
 import sys
 import os
+import getopt
 import Skype4Py
 import prowlpy
 
-PROWL_API_KEY	= "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+PROWL_API_KEY	    = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+OWN_DISPLAY_NAME  = ""
 
 skype 			= Skype4Py.Skype()
 prowl 			= prowlpy.Prowl(PROWL_API_KEY)
+
+help_message = '''
+The help message goes here.
+'''
 
 # ----------------------------------------------------------------------------------------------------
 # Fired on attachment status change. Here used to re-attach this script to Skype in case attachment is lost. Just in case.
@@ -29,10 +35,11 @@ def OnAttach(status):
     print('******************************************************************************');
 
 def OnMessageStatus(Message, Status):
-	if (Status == 'RECEIVED' or Status == 'SENT'):
+	if Status == 'RECEIVED' or Status == 'SENT':
 		topic = Message.Chat.Topic[:10]
-		if Status == 'RECEIVED':
-			message = Message.FromDisplayName + ': ' + Message.Body
+		displayName = Message.FromDisplayName
+		if Status == 'RECEIVED' and displayName != OWN_DISPLAY_NAME:
+			message = displayName + ': ' + Message.Body
 			sendNotification('Skype', topic, message)
 		if Status == 'SENT':
 			message = 'Myself: ' + Message.Body
@@ -46,10 +53,42 @@ def sendNotification(appname, event, description):
 	except Exception,msg:
 	  print msg
 
-def main():
+class Usage(Exception):
+  def __init__(self, msg):
+    self.msg = msg
+
+def main(argv=None):
+  if argv is None:
+    argv = sys.argv
+  try:
+    try:
+      opts, args = getopt.getopt(argv[1:], "hu:k:v", ["help", "user", "api_key"])
+    except getopt.error, msg:
+      raise Usage(msg)
+  
+    # option processing
+    for option, value in opts:
+      if option == "-v":
+        verbose = True
+      if option in ("-h", "--help"):
+        raise Usage(help_message)
+      if option in ("-u", "--user"):
+        global OWN_DISPLAY_NAME
+        OWN_DISPLAY_NAME = value
+      if option in ("-k", "--api_key"):
+        global PROWL_API_KEY
+        PROWL_API_KEY = value
+      
+  except Usage, err:
+    print >> sys.stderr, sys.argv[0].split("/")[-1] + ": " + str(err.msg)
+    print >> sys.stderr, "\t for help use --help"
+    return 2
+  
   skype.OnAttachmentStatus = OnAttach;
   skype.OnMessageStatus = OnMessageStatus;
   print('******************************************************************************');
+  print 'Username      : ' + OWN_DISPLAY_NAME
+  print 'Prowl API Key : ' + PROWL_API_KEY
   print 'Connecting to Skype..'
   skype.Attach();
   
@@ -58,7 +97,7 @@ def main():
   Cmd = '';
   while not Cmd == 'exit':
     pass
-    #Cmd = raw_input('');
+    Cmd = raw_input('');
 
 if __name__ == '__main__':
   main()
