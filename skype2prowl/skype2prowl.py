9c20b94218f8a5ea838861fd583d36a01fee53ff
drikin <drikin@gmail.com>
2009-08-05 21:45:14 -0700

    supress notification within timedelta

--- skype2prowl/skype2prowl.py
+++ skype2prowl/skype2prowl.py
@@ -10,15 +10,19 @@ Copyright (c) 2009 drikin.com. All rights reserved.
 import sys
 import os
 import getopt
+import datetime
 import Skype4Py
 import prowlpy
 
-PROWL_API_KEY	    = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+PROWL_API_KEY     = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 OWN_DISPLAY_NAME  = ""
+lastTime          = datetime.datetime.now()
+deltaTime         = datetime.timedelta(minutes=1)
+#deltaTime         = datetime.timedelta(seconds=30)
 
-skype 			= Skype4Py.Skype()
-prowl 			= prowlpy.Prowl(PROWL_API_KEY)
+skype       = Skype4Py.Skype()
 
+verbose     = False
 help_message = '''
 The help message goes here.
 '''
@@ -27,31 +31,42 @@ The help message goes here.
 # Fired on attachment status change. Here used to re-attach this script to Skype in case attachment is lost. Just in case.
 def OnAttach(status):
   print 'API attachment status: ' + skype.Convert.AttachmentStatusToText(status)
-
+  
   if status == Skype4Py.apiAttachAvailable:
     skype.Attach();
-
+  
   if status == Skype4Py.apiAttachSuccess:
     print('******************************************************************************');
 
 def OnMessageStatus(Message, Status):
-	if Status == 'RECEIVED' or Status == 'SENT':
-		topic = Message.Chat.Topic[:10]
-		displayName = Message.FromDisplayName
-		if Status == 'RECEIVED' and displayName != OWN_DISPLAY_NAME:
-			message = displayName + ': ' + Message.Body
-			sendNotification('Skype', topic, message)
-		if Status == 'SENT':
-			message = 'Myself: ' + Message.Body
+  if Status == 'RECEIVED' or Status == 'SENT':
+    global lastTime
+    time        = Message.Datetime
+    if( time - lastTime > deltaTime):
+      topic       = Message.Chat.Topic[:10]
+      displayName = Message.FromDisplayName
+      if( not topic ):
+        topic = displayName
+      
+      if Status == 'RECEIVED' and displayName != OWN_DISPLAY_NAME:
+        message = displayName + ': ' + Message.Body
+        sendNotification('Skype', topic, message)
+      if Status == 'SENT':
+        message = 'Myself: ' + Message.Body
+        if( verbose ):
+          print message
+    
+    lastTime = time
 
 def sendNotification(appname, event, description):
-	if( not PROWL_API_KEY ):
-		return
-	try:
-		print appname + ' ' + event + ': ' + description
-		prowl.add(appname.encode('utf-8'), event.encode('utf-8'), description.encode('utf-8'))
-	except Exception,msg:
-	  print msg
+  if( not PROWL_API_KEY ):
+    return
+  try:
+    if( verbose ):
+      print appname + ' ' + event + ': ' + description
+    prowl.add(appname.encode('utf-8'), event.encode('utf-8'), description.encode('utf-8'))
+  except Exception,msg:
+    print msg
 
 class Usage(Exception):
   def __init__(self, msg):
@@ -65,10 +80,11 @@ def main(argv=None):
       opts, args = getopt.getopt(argv[1:], "hu:k:v", ["help", "user", "api_key"])
     except getopt.error, msg:
       raise Usage(msg)
-  
+    
     # option processing
     for option, value in opts:
       if option == "-v":
+        global verbose
         verbose = True
       if option in ("-h", "--help"):
         raise Usage(help_message)
@@ -78,7 +94,7 @@ def main(argv=None):
       if option in ("-k", "--api_key"):
         global PROWL_API_KEY
         PROWL_API_KEY = value
-      
+  
   except Usage, err:
     print >> sys.stderr, sys.argv[0].split("/")[-1] + ": " + str(err.msg)
     print >> sys.stderr, "\t for help use --help"
@@ -90,7 +106,10 @@ def main(argv=None):
   print 'Username      : ' + OWN_DISPLAY_NAME
   print 'Prowl API Key : ' + PROWL_API_KEY
   print 'Connecting to Skype..'
+  
   skype.Attach();
+  global prowl
+  prowl = prowlpy.Prowl(PROWL_API_KEY)
   
   # ----------------------------------------------------------------------------------------------------
   # Looping until user types 'exit'
